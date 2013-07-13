87dee0a7977c895da2fa7d71086667f5384aa9a4
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-07-13 12:44:37 -0500

    initial commit

---
+++ jukebox.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+from subprocess import call
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def jukebox():
+    if request.method == 'POST':
+        url = request.form['url']
+        #vlc_param = '"' + url + '"'
+        print url
+        call(["vlc", url])
+    return render_template('jukebox.html')
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
