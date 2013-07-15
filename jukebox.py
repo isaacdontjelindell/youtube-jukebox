20b2750d5f0afb31db848d19bded74ca21614b7a
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-07-15 11:28:29 -0500

    use telnet to control VLC

--- jukebox.py
+++ jukebox.py
@@ -3,16 +3,49 @@ from flask import request
 from flask import render_template
 
 from subprocess import call
+from subprocess import Popen
+from Queue import Queue
 
+import sys
+import telnetlib
+import time
+
+# start VLC with the telnet control interface enabled
+vlc_pid = Popen(['vlc', '--extraintf', 'telnet'])
+time.sleep(1)
+
+# make a connection to the telnet interface
+tn = telnetlib.Telnet('localhost', 4212)
+tn.read_until("Password:")
+tn.write('jukebox\n')
+time.sleep(.5)
+
+# will hold songs to be played
+play_queue = Queue()
+
+# start the Flask listener
 app = Flask(__name__)
+
+#tn.write("enqueue http://www.youtube.com/watch?v=L4ekUKIdzrs\n")
+#time.sleep(.5)
+#tn.write("play\n")
+#time.sleep(1)
+#tn.write("vtrack -1\n") # disable video
 
 @app.route('/', methods=['POST', 'GET'])
 def jukebox():
     if request.method == 'POST':
         url = request.form['url']
-        print url + "&amp;vq=small"
-        call(["vlc", " --intf telnet", '''--lua-config "telnet={host='0.0.0.0:4212'}"''', "--novideo", url])
+        url = url + "&amp;vq=small"
+        enqueue(url)
     return render_template('jukebox.html')
-    
+
+def enqueue(url):
+    tn.write('enqueue ' + url + '\n')
+    time.sleep(0.5)
+    tn.write('play\n')
+    time.sleep(1)
+    tn.write("vtrack -1\n") # disable video
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
