5d6778024085d56c01fa37f8d483cb8fd87427f9
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-08-03 14:41:50 -0500

    start rewrite using selenium

--- jukebox.py
+++ jukebox.py
@@ -1,42 +1,30 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-
-from subprocess import Popen
-
-import telnetlib
-import time
-
-# start VLC with the telnet control interface enabled
-vlc_pid = Popen(['vlc', '--extraintf', 'telnet'])
-time.sleep(1)
-
-# make a connection to the telnet interface
-tn = telnetlib.Telnet('localhost', 4212)
-tn.read_until("Password:")
-tn.write('jukebox\n')
-time.sleep(.5)
-
-playing = False
+from selenium import webdriver
+import Queue
 
 # start the Flask listener
 app = Flask(__name__)
+queue = Queue.Queue()
+driver = webdriver.Firefox()
 
 @app.route('/', methods=['POST', 'GET'])
 def jukebox():
     if request.method == 'POST':
-        url = request.form['url']
-        url = url + "&amp;vq=small"
-        enqueue(url)
+        videoid = request.form['url']
+        enqueue(videoid)
+
     return render_template('jukebox.html')
 
-def enqueue(url):
-    global playing
-    tn.write('enqueue ' + url.encode('ascii') + '\n')
-    if not playing:
-        time.sleep(0.5)
-        tn.write('play\n')
-        playing = True
+@app.route('/player/<videoid>', methods=['GET'])
+def player(videoid):
+    return render_template('player.html', video_id=videoid)
+
+def enqueue(videoid):
+    driver.get("http://localhost:5000/player/"+videoid)
+    pass
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', threaded=True)
+    #app.run(host='0.0.0.0', debug=True)
