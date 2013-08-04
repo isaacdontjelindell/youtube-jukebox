dacf5d90362b1793db1a97cec071ec9ccec172a6
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-08-04 13:17:56 -0500

    basic queuing functionality

--- jukebox.py
+++ jukebox.py
@@ -2,24 +2,37 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from selenium import webdriver
-import Queue
+from Queue import Queue
+import urlparse
+
 
 # start the Flask listener
 app = Flask(__name__)
-queue = Queue.Queue()
+
+# open the browser
 driver = webdriver.Firefox()
+
+# play queue - contains video ids to be played
+queue = Queue()
 
 @app.route('/', methods=['POST', 'GET'])
 def jukebox():
     if request.method == 'POST':
-        videoid = request.form['url']
-        enqueue(videoid)
+        url = request.form['url']
+        enqueue(url)
 
     return render_template('jukebox.html')
 
-def enqueue(videoid):
-    driver.get("http://localhost:5000/player/"+videoid)
-    pass
+def enqueue(url):
+    # parse the url to get the video id
+    # http://www.youtube.com/watch?v=39RrLTNyGTM
+    url_data = urlparse.urlparse(url)
+    query = urlparse.parse_qs(url_data.query)
+    video_id = query["v"][0]
+    queue.put(video_id)
+
+    #if not 'player' in driver.current_url:
+    #    driver.get("http://localhost:5000/player")
 
 @app.route('/pause', methods=['POST'])
 def pause():
@@ -36,8 +49,17 @@ def play():
 def player(videoid):
     return render_template('player.html', video_id=videoid)
 
+@app.route('/player', methods=['GET'])
+def player():
+    return render_template('player.html', video_id=None)
 
+@app.route('/getnextvideo', methods=['POST'])
+def getnextvideo():
+    vid = queue.get()
+    queue.task_done()
+    return vid
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True)
     #app.run(host='0.0.0.0', debug=True)
+
